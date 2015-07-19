400125c55b87ea80d7eb9e31fcb5384255858240
kevin8108271 <102304032@nccu.edu.tw>
2015-07-20 02:00:47 +0800

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,7 @@
+data<-read.table("household_power_consumption.txt",header=T,sep=';')
+use<-subset(data,data$Date=="1/2/2007" | data$Date== "2/2/2007")
+png(file='plot1.png',height=480,width=480)
+hist(use$Global_active_power,mani='Global Active Power',xlab='Global Active Power(kilowatts)')
+dev.off()
+
+
